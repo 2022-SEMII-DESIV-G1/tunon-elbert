591c3826020226bcf53e0da85384fa9bcf4af9d4
Elbert Tuñon <jtg145@homail.com>
2022-11-26 16:11:06 -0500

    Laboratorio10

---
+++ Laboratorio10/laboratorio10/src/main/java/com/sample/App.java
@@ -0,0 +1,9 @@
+package com.sample;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
